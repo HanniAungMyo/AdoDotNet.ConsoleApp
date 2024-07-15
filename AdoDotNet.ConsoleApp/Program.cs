215eba511b622cade4229f92e428d3b646dfc7a4
HanniAungMyo <hsuhanni095@gmail.com>
2024-07-15 19:45:04 +0630

    Add project files.

---
+++ AdoDotNet.ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = "LAPTOP\\SQLSERVER";
+sqlConnectionStringBuilder.InitialCatalog = "Student";
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "sa@123";
+
+SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+Console.WriteLine(sqlConnectionStringBuilder.ConnectionString);
+Console.WriteLine("Opening Connection");
+
+sqlConnection.Open();
+Console.WriteLine("Open Connection");
+
+//Console.WriteLine("Connection Open");
+string query = @"SELECT [Id],
+[StuName],
+[FatherName],
+[StuContent]
+FROM [dbo].[Tbl_Stu]";
+SqlCommand cmd = new SqlCommand(query,sqlConnection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+
+Console.WriteLine("Closing Connection");
+
+sqlConnection.Close();
+Console.WriteLine("Close Connection");
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Student Id =>" + dr["Id"]);
+    Console.WriteLine("Student Name =>" + dr["StuName"]);
+    Console.WriteLine("Father Name =>" + dr["FatherName"]);
+    Console.WriteLine("Student Content =>" + dr["StuContent"]);
+    Console.WriteLine("-------------------------------------------------------------------------------------------");
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,27 @@
+C#
+.net Framework (Windows)
+
+.net Core (Windows,Linux,Mac)
+.1
+.2
+.2.2
+.3
+.3.1
+.4
+
+
+.net
+.5 (vs19,vs2022)
+.6(vscode vs2022)
+.7(current)
+.8
+.9(Beta)
+
+.console App
+.Window Form
+.asp.net web Forms
+.asp.net core  web mvc Forms
+.asp.net core web api
+
+
+
