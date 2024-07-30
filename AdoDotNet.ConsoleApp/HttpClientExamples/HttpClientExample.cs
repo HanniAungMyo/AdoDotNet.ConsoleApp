21dcc9a87a7b80d35fefe94ada78b86a73202b88
HanniAungMyo <hsuhanni095@gmail.com>
2024-07-30 12:31:57 +0630

    [Complete]HttpClient CRUD

--- AdoDotNet.ConsoleApp/HttpClientExamples/HttpClientExample.cs
+++ AdoDotNet.ConsoleApp/HttpClientExamples/HttpClientExample.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,8 +13,15 @@ namespace AdoDotNet.ConsoleApp.HttpClientExamples
     {
         public async Task Run()
         {
-            await Read();
+            await Delete(1);
+            //await Create("Aye Aye", "Min Thein", "Aye Aye is Electronic Student");
+            //await Read();
+            //await Update(22, "Su Su", "Tin Aye", "Su Su is English Student");
+
+
+
         }
+
         private async Task Read()
 
         {
@@ -36,7 +44,89 @@ namespace AdoDotNet.ConsoleApp.HttpClientExamples
             }
         }
 
+        public async Task Edit(int id)
+        {
+            string url = $"https://localhost:7288/api/Student/{id}";
+            HttpClient Client = new HttpClient();
+            HttpResponseMessage response = await Client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                StudentModel student = JsonConvert.DeserializeObject<StudentModel>(jsonStr)!;
+                Console.WriteLine(student.Id);
+                Console.WriteLine(student.StuName);
+                Console.WriteLine(student.FatherName);
+                Console.WriteLine(student.StuContent);
+            }
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
 
+        public async Task Create(string stuName, string fatherName, string stuContent)
+        {
+            StudentModel student = new StudentModel()
+            {
+                StuName = stuName,
+                FatherName = fatherName,
+                StuContent = stuContent
+            };
+            string jsonStudent = JsonConvert.SerializeObject(student);
+            HttpContent httpContent = new StringContent(jsonStudent, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+            string url = $"https://localhost:7288/api/Student";
+            HttpClient Client = new HttpClient();
+            HttpResponseMessage response = await Client.PostAsync(url, httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+            }
+        }
+
+        public async Task Update(int id, string stuName, string fatherName, string stuContent)
+        {
+            StudentModel student = new StudentModel()
+            {
+                Id = id,
+                StuName = stuName,
+                FatherName = fatherName,
+                StuContent = stuContent
+            };
+            string jsonStudent = JsonConvert.SerializeObject(student);
+            HttpContent httpContent = new StringContent(jsonStudent, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+            string url = $"https://localhost:7288/api/Student/{id}";
+            HttpClient Client = new HttpClient();
+            HttpResponseMessage response = await Client.PutAsync(url, httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+            }
+
+        }
+
+        public async Task Delete(int id)
+        {
+            string url = $"https://localhost:7288/api/Student/{id}";
+            HttpClient Client = new HttpClient();
+            HttpResponseMessage response = await Client.DeleteAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+            }
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+            }
+        }
 
     }
 }
